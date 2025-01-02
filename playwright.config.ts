d1745caa10cc26be4a87516237702cdc04f76266
Juan David Arroyave Duque <juan.duque140803@gmail.com>
2025-01-02 10:21:00 +0100

    added headless node1

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
